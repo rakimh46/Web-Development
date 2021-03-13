c4a1b6dc0e90720253e08e3d96f9c7fb53dcde9b
rakimh46 <60341490+rakimh46@users.noreply.github.com>
2021-03-13 14:51:58 +0530

    lbc

---
+++ 6_Webscrapping_13_02_2021/raw/poc/lbc.js
@@ -0,0 +1,36 @@
+// npm init -y
+//npm install request cheerio
+
+let cheerio = require("cheerio");
+let request = require("request");
+let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/ball-by-ball-commentary";
+request(url, cb);
+console.log("before");
+function cb(error, response, html) {
+    if (error) {
+        throw error;
+    } else {
+        extractHtml(html);
+    }
+}
+
+function extractHtml(html) {
+    let selectorTool = cheerio.load(html);
+    let allCommentaries = selectorTool(".d-flex.match-comment-padder.align-items-center .match-comment-long-text");
+
+    // another ex
+    //let allCommentaries = selectorTool(".sidebar-widget-coverage-item .content .title");
+
+    console.log(allCommentaries.length);
+    //rule -> index, cheerioselector
+    // for (let i = 0; i < allCommentaries.length; i++) {
+    //     let lastbComment = selectorTool(allCommentaries[i]).text();
+    //     console.log(lastbComment);
+    //     console.log("------------------");
+    // }
+
+    let lastbComment = selectorTool(allCommentaries[0]).text();
+    console.log(lastbComment);
+
+}
+console.log("after");
--- 6_Webscrapping_13_02_2021/raw/poc/request.js
+++ 6_Webscrapping_13_02_2021/raw/poc/request.js
@@ -1,6 +1,7 @@
 // npm init -y
 //npm install request cheerio
 
+const cheerio = require("cheerio");
 let request=require("request");
 request("https://www.google.com",cb);
 console.log("before");
@@ -8,7 +9,14 @@ function cb(error,response,html){
     if(error) {
         throw error;
     }else { 
-        console.log(html);
+        extractHtml(html);
     }
+}
+
+function extractHtml(html){
+    let selectorTool = cheerio.load(html);
+    let selectElem  = selectorTool("#SIvCob");
+    console.log(selectElem.text());
+    console.log(selectElem.html());
 }
 console.log("after");
